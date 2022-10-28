56a313fa01a4bd773808512e47a18e99f8db4fbf
figaroltgit config --global user.email logrez@gmail.comgit add .git statuscd vgzgit config --global user.name figaroltgit config --global user.name figarolt <logrez@gmail.com>
2022-10-29 00:11:57 +0300

    1 task done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,36 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+
+document.querySelector('form').addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const kilograms = document.querySelector('#weightInput').value;
+    const pounds = document.createElement('h4');
+    const grams = document.createElement('h4');
+    const ounces = document.createElement('h4');
+
+    pounds.style.backgroundColor = 'lightGreen';
+    pounds.style.padding = '10px';
+    pounds.textContent = "POUNDS:" + " " + kilograms * 2.2046;
+    grams.style.backgroundColor = 'lightBlue';
+    grams.style.padding = '10px';
+    grams.textContent = "GRAMS:" + " " + kilograms / 0.0010000;
+    ounces.style.backgroundColor = 'pink';
+    ounces.style.padding = '10px';
+    ounces.textContent = "OUNCES:" + " " + kilograms * 35.274;
+
+    const weightOutput = document.querySelector('#output');
+
+    weightOutput.style.height = 'auto';
+    weightOutput.style.width = '500px';
+    weightOutput.style.textAlign = 'center';
+    weightOutput.style.margin = 'auto';
+
+
+
+weightOutput.append(pounds, grams, ounces);
+
+    
+})
