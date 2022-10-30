e4c7404e77a7c9485e77ad9427f930f8260fe84b
figaroltgit config --global user.email logrez@gmail.comgit add .git statuscd vgzgit config --global user.name figaroltgit config --global user.name figarolt <logrez@gmail.com>
2022-10-30 21:52:58 +0200

    Task 8 complete

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+
+  this.sum = function () {
+    return a + b;
+  };
+  this.substraction = function () {
+    return a - b;
+  };
+  this.multiplication = function () {
+    return a * b;
+  };
+  this.division = function () {
+    return a / b;
+  };
+}
+
+const calculator = new Calculator(10, 2);
+
+console.log(
+  `Sum result: ${calculator.sum()}`,
+  `Substraction result: ${calculator.substraction()}`,
+  `Multiplication result: ${calculator.multiplication()}`,
+  `Division result: ${calculator.division()}`
+);
